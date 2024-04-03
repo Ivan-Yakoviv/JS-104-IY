648799c53afde30e7b0161fa1fa03452fac44b1b
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-04 00:08:56 +0300

    Update task-15.js

--- js/task-15.js
+++ js/task-15.js
@@ -25,3 +25,68 @@ console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
 const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
 const newClients = ["Peach", "Houston"];
 const allClients = oldClients.concat(newClients);
+
+const propName = "name";
+const user = {
+  age: 25,
+};
+
+
+user[propName] = "Henry Sibola";
+console.log(user); // "Henry Sibola"
+
+// function getAllPropValues(propName) {
+//     const products = [
+//         { name: "Radar", price: 1300, quantity: 4 },
+//         { name: "Scanner", price: 2700, quantity: 3 },
+//         { name: "Droid", price: 400, quantity: 7 },
+//         { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+
+
+//     const values = [];
+
+
+//     for (const product of products) {
+//        if (Object.keys(product).includes(propName)) {
+//            values.push(product[propName]);
+//         }
+//     }return values;
+// }
+    
+// console.log (getAllPropValues("price")) ;
+
+function calculateTotalPrice(productName) {
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+    let totalPrice;
+   for (const product of products) {
+      if (product[propName] === productName) {
+        totalPrice = product.price*product.quantity;
+       return totalPrice;
+      }
+    }
+    return `"Product ${productName} not found!"`;
+}
+
+console.log(calculateTotalPrice("Gri"));
+
+const bookShelf = {
+  books: [
+		{ title: "The Last Kingdom", rating: 8 }, 
+		{ title: "The Mist", rating: 6 }
+	],
+  getBooks() {
+    return this.books;
+  },
+  addBook(newBook) {
+    this.books.push(newBook);
+  }
+};
+
+bookShelf.addBook({ title: "Dream Guardian", rating: 9 });
+console.log(bookShelf);
