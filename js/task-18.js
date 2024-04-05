816f70ed03a830acc24ede50173f6b23a724a6b3
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-05 23:51:24 +0300

    ljhg

---
+++ js/task-18.js
@@ -0,0 +1,47 @@
+// До нас звернулася власниця крамниці зілля «У старої жаби» 
+// і замовила програму для ведення інвентарю.Програма має додавати, видаляти, шукати та оновлювати зілля.
+
+// Оголоси об'єкт atTheOldToad з наступними властивостями:
+
+// potions — масив, де будуть зберігатися зілля. Нехай зараз він буде порожнім
+// getPotions() — метод, який повертає рядок "List of all available potions"
+// addPotion(potionName) — метод, який повертає рядок "Adding <potionName>", 
+// де potionName — це значення параметра potionName
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Зміни код метода об'єкта getPotions() так, щоб він повертав значення властивості potions
+  getPotions() {
+    return this.potions;
+  },
+  // Зміни код методу addPotion(potionName) так, щоб він додавав зілля potionName
+  // в кінець масиву у властивості potions.
+  addPotion(newPotion) {
+    this.potions.push(newPotion);
+  },
+  // Додай метод getTotalPrice(), який має повертати загальну вартість усіх зілль з властивості potions.
+  getTotalPrice() {
+    let total = 0;
+    for (const potion of this.potions) {
+      total += potion.price;
+    } 
+    return total;
+  },
+  // Доповни метод updatePotionName(oldName, newName) таким чином,
+  // щоб він оновлював назву зілля з oldName на newName в масиві зілля у властивості potions.
+  updatePotionName(oldName, newName) {
+    for (const potion of this.potions) {
+      if (potion.name === oldName) {
+        potion.name = newName;
+      }
+    }
+  },
+
+};
+atTheOldToad.updatePotionName("Stone skin", "Invisibility");
+console.log(atTheOldToad.getTotalPrice());
+console.log(atTheOldToad.updatePotionName("Stone skin", "Invisibility"));
+
