8e89a332daa2d118c2b6b9f3e2f94bdff53a3456
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-12 22:15:49 +0300

    Update task-21.js

--- js/task-21.js
+++ js/task-21.js
@@ -1,19 +1,157 @@
-const allCars = [
-  { make: "Honda", model: "CR-V", type: "suv", amount: 14, price: 24045, onSale: true},
-  { make: "Honda", model: "Accord", type: "sedan", amount: 2, price: 22455, onSale: true },
-  { make: "Mazda", model: "Mazda 6", type: "sedan", amount: 8, price: 24195, onSale: false },
-  { make: "Mazda", model: "CX-9", type: "suv", amount: 7, price: 31520, onSale: true },
-  { make: "Toyota", model: "4Runner", type: "suv", amount: 19, price: 34210, onSale: false },
-  { make: "Toyota", model: "Sequoia", type: "suv", amount: 16, price: 45560, onSale: false },
-  { make: "Toyota", model: "Tacoma", type: "truck", amount: 4, price: 24320, onSale: true },
-  { make: "Ford", model: "F-150", type: "truck", amount: 11, price: 27110, onSale: true },
-  { make: "Ford", model: "Fusion", type: "sedan", amount: 13, price: 22120, onSale: true },
-  { make: "Ford", model: "Explorer", type: "suv", amount: 6, price: 31660, onSale: false }
+// const allCars = [
+//   { make: "Honda", model: "CR-V", type: "suv", amount: 14, price: 24045, onSale: true},
+//   { make: "Honda", model: "Accord", type: "sedan", amount: 2, price: 22455, onSale: true },
+//   { make: "Mazda", model: "Mazda 6", type: "sedan", amount: 8, price: 24195, onSale: false },
+//   { make: "Mazda", model: "CX-9", type: "suv", amount: 7, price: 31520, onSale: true },
+//   { make: "Toyota", model: "4Runner", type: "suv", amount: 19, price: 34210, onSale: false },
+//   { make: "Toyota", model: "Sequoia", type: "suv", amount: 16, price: 45560, onSale: false },
+//   { make: "Toyota", model: "Tacoma", type: "truck", amount: 4, price: 24320, onSale: true },
+//   { make: "Ford", model: "F-150", type: "truck", amount: 11, price: 27110, onSale: true },
+//   { make: "Ford", model: "Fusion", type: "sedan", amount: 13, price: 22120, onSale: true },
+//   { make: "Ford", model: "Explorer", type: "suv", amount: 6, price: 31660, onSale: false }
+// ];
+
+// const getAvailableCarNames = (cars) => cars
+//   .filter(car => car.onSale)
+//   .map(lalala => lalala.model);
+
+
+// console.log(getAvailableCarNames(allCars));
+
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     balance: 2811
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     balance: 3821
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     balance: 3793
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     balance: 2278
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     balance: 3951
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     balance: 1498
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     balance: 2764
+//   },
+// ]
+
+// const getUserNames = (users) => users.map(user => user.name);
+
+// console.log(getUserNames(users));
+
+
+// const allUsers = [
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"]
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"]
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ];
+
+// const getUsersWithFriend = (users, friendName) =>
+//   users.filter(user => user.friends.includes(friendName));
+
+// console.log(getUsersWithFriend(allUsers, "Briana Decker"));
+// console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// console.log(getUsersWithFriend(allUsers, "Adrian Cross"));
+  
+
+// const sortByDescendingFriendCount = (users) =>
+//   users.toSorted((a, b) => b.friends.length - a.friends.length);
+
+//   console.log(sortByDescendingFriendCount(allUsers));
+
+
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
 ];
 
-const getAvailableCarNames = (cars) => cars
-  .filter(car => car.onSale)
-  .map(lalala => lalala.model);
+const getTotalBalanceByGender = (users, gender) => {
+  let total = 0;
+  users
+    .filter(user => user.gender === gender)
+    .forEach(client => { total += client.balance });
+  return total;
+}
 
+console.log(getTotalBalanceByGender(clients, "male")); 
 
-console.log(getAvailableCarNames(allCars));
+console.log(getTotalBalanceByGender(clients, "female")); 
