1feadc289f3b3caa07f4fef119eb44e5363c21d8
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-13 22:37:31 +0300

    add 22

--- js/task-22.js
+++ js/task-22.js
@@ -1,14 +1,26 @@
-const players = {
-  mango: 1270,
-  poly: 468,
-  ajax: 710,
-  kiwi: 244,
-};
-const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244,
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
 
-const totalPlayTime = playtimes.reduce((acc, playtime) => 
-     acc + playtime, 0);
+// const totalPlayTime = playtimes.reduce((acc, playtime) => 
+//      acc + playtime, 0);
 
-const averagePlayTime = totalPlayTime / playtimes.length;
+// const averagePlayTime = totalPlayTime / playtimes.length;
 
-console.log(totalPlayTime);
+// console.log(totalPlayTime);
+
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+
+const totalAveragePlaytimePerGame = players.reduce((acc, player) => acc + player.playtime / player.gamesPlayed, 0);
+    
+
+console.log(totalAveragePlaytimePerGame);
