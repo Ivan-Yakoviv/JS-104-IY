8a540878374d606f04a04dff20a5fbbabb07d266
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-04-18 00:01:50 +0300

    Update task-24.js

--- js/task-24.js
+++ js/task-24.js
@@ -1,12 +1,68 @@
-const parent = {
-  name: "Stacey",
-  surname: "Moore",
-  age: 54,
-  heritage: "Irish",
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+
+// console.log(child);
+
+// const objC = { c: "objC prop" };
+
+// const objB = Object.create(objC);
+// objB.b = "objB prop";
+
+// const objA = Object.create(objB);
+// objA.a = "objA prop";
+
+// console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+// console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+// console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
+
+// console.log(objA.hasOwnProperty("a")); // true
+// console.log(objA.a); // "objA prop"
+
+// console.log(objA.hasOwnProperty("b")); // false
+// console.log(objA.b); // "objB prop"
+
+// console.log(objA.hasOwnProperty("c")); // false
+// console.log(objA.c); // "objC prop"
+
+// console.log(objA.hasOwnProperty("x")); // false
+// console.log(objA.x); // undefined
+
+// class User {
+//   constructor() {
+// 		console.log("constructor call")
+//   }
+// }
+
+// const mango = new User(); // "constructor call"
+// console.log(mango); // {}
+
+// class Car {
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+// }
+
+// console.log(new Car("Audi", "Q3", 36000));
+
+class Car {
+  constructor(param) {
+    this.brand = param.brand;
+    this.model = param.model;
+    this.price = param.price;
+  }
 };
 
-const child = Object.create(parent);
-child.name = "Jason";
-child.age = 27;
+console.log(new Car({ brand: "Audi", model: "Q3", price: 36000 }));
 
-console.log(child);
+
+
