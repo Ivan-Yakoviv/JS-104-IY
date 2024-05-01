fbca6ca99bb85bf65357a3e91d9991ef0e8cdd5d
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-05-01 21:21:54 +0300

    add 28

---
+++ js/task-28.js
@@ -0,0 +1,7 @@
+// const instance = basicLightbox.create(`
+//     <h1>Dynamic Content</h1>
+//     <p>You can set the content of the lightbox with JS.</p>
+// `);
+// instance.show();
+
+console.log(_.sum([4, 2, 8, 6])); // 20
