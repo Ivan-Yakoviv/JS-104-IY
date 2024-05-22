c56f1466ac24d2b65b23e6f58861cfb16c00f5de
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-05-22 21:33:36 +0300

    098

---
+++ js/task-32.js
@@ -0,0 +1,12 @@
+fetch("<https://jsonplaceholder.typicode.com/users>")
+  .then(response => {
+    // Response handling
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
+
+  
